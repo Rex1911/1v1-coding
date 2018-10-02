6a5a05f817e7bf11f2838f2c66e247e546f97693
Aryan Likhite <aryanlikhite@gmail.com>
2018-10-02 11:38:06 +0530

    Removed comment in admin.js

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -27,36 +27,3 @@ $("#formSubmit").click(function(){
         }
     })
 })
-
-// $("#1").click(() => {
-//     let questionID = $("#1_select").val();
-//     socket.emit("startContest", 1);
-// });
-
-// $("#2").click(() => {
-//     socket.emit("startContest", 2);
-// });
-
-// $("#3").click(() => {
-//     socket.emit("startContest", 3);
-// });
-
-// $("#4").click(() => {
-//     socket.emit("startContest", 4);
-// });
-
-// $("#5").click(() => {
-//     socket.emit("startContest", 5);
-// });
-
-// $("#6").click(() => {
-//     socket.emit("startContest", 6);
-// });
-
-// $("#7").click(() => {
-//     socket.emit("startContest", 7);
-// });
-
-// $("#8").click(() => {
-//     socket.emit("startContest", 8);
-// });
